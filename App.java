84f2e0ab5f0015710d43213a2db859fb6c718136
Mario González <gonzalezlozanomario27@gmail.com>
2024-03-05 12:12:21 +0100

    Clase MenuInicial creado y App mofificado

--- App.java
+++ App.java
@@ -1,5 +1,16 @@
+import java.awt.*;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hola");
+        int opcion;
+        Scanner in = new Scanner(System.in);
+        //Sacamos el menú de inicio de sesión
+        MenuInicial.Escribir();
+        opcion = Integer.parseInt(in.nextLine());
+        if(opcion==1){
+            System.out.println("Introduce la contraseña");
+        }
+
     }
 }
---
+++ MenuInicial.java
@@ -0,0 +1,8 @@
+public class MenuInicial {
+    protected static void Escribir(){
+        System.out.println("Elige un inicio de sesión\n" +
+                            "1-Login de Administrador\n" +
+                            "2-Login de Departamento");
+
+    }
+}
