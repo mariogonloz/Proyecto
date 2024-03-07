45a1e947894972159425e054737c148fff123439
Mario González <gonzalezlozanomario27@gmail.com>
2024-03-07 09:58:49 +0100

    Dia 7-3-2024 inicio

--- App.java
+++ App.java
@@ -2,16 +2,9 @@ import java.awt.*;
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) {
-        int opcion;
-        int opcionAdmin = 0;
-        final String contr = "ADMIN";
-        String contrIntroducida;
-        boolean error = false;
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args){
 
-        /*Sacamos el menú de inicio de sesión y nos aseguramos que el usuario solo
-        introduce valores numéricos entre 1 y 3*/
+        /*Sacamos el menú de inicio de sesión*/
         do {
             MenuInicial.Escribir();
             try {
@@ -22,8 +15,7 @@ public class App {
                         do {
                             System.out.println("Introduce la contraseña");
                             contrIntroducida = in.nextLine();
-                        /*Si la contraseña es correcta, mostramos el menú de admin, si no,
-                        mostrará un mensaje de contraseña errónea y volverá a preguntar la contraseña*/
+
                             if (contrIntroducida.equals(contr)) {
                                 MenuAdministrador.Escribir();
                                 try {
@@ -63,8 +55,7 @@ public class App {
                             }
 
                         } while (!contrIntroducida.equals(contr));
-                    } else if (opcion == 2) {
-                        System.out.println("Introduce el código del departamento");
+
 
                     }
                 } while (opcion != 3);
--- MenuInicial.java
+++ MenuInicial.java
@@ -1,8 +1,6 @@
 import java.util.Scanner;
 
 public class MenuInicial {
-
-
     protected static void Escribir() {
         int opcion;
         boolean error = false;
@@ -14,34 +12,36 @@ public class MenuInicial {
                 "1-Login de Administrador\n" +
                 "2-Login de Departamento\n" +
                 "3-Salir");
-        try {
-            do {
-                error = false;
-                opcion = Integer.parseInt(in.nextLine());
-                if (opcion == 1) {
-                    do {
-                        System.out.println("Introduce la contraseña");
-                        contrIntroducida = in.nextLine();
+        /*Nos aseguramos que el usuario solo introduce valores numéricos entre 1 y 3*/
+        do {
+            try {
+                do {
+                    error = false;
+                    opcion = Integer.parseInt(in.nextLine());
+                    if (opcion == 1) {
                         /*Si la contraseña es correcta, mostramos el menú de admin, si no,
                         mostrará un mensaje de contraseña errónea y volverá a preguntar la contraseña*/
-                        if (contrIntroducida.equals(contr)) {
-                            MenuAdministrador.Escribir();
-                        } else {
-                            System.out.println("Contraseña incorrecta, pulsa intro para volver a " +
-                                    "introducir la contraseña");
-                            in.nextLine();
-                        }
+                        do {
+                            System.out.println("Introduce la contraseña");
+                            contrIntroducida = in.nextLine();
+
+                            if (contrIntroducida.equals(contr)) {
+                                MenuAdministrador.Escribir();
+                            } else {
+                                System.out.println("Contraseña incorrecta, pulsa intro para volver a " +
+                                        "introducir la contraseña");
+                                in.nextLine();
+                            }
+                        } while (!contrIntroducida.equals(contr));
+                    } else if (opcion == 2) {
+                        System.out.println("Introduce el código del departamento");
                     }
-                }
-            }while(opcion!=3);
-
-
-        } catch (NumberFormatException nfe) {
-            System.out.println("Opción no válida, pulsa intro para volver a elegir una opción");
-            in.nextLine();
-            error = true;
-        } while (!contrIntroducida.equals(contr)) ;
+                } while (opcion != 3);
+            } catch (NumberFormatException nfe) {
+                System.out.println("Opción no válida, pulsa intro para volver a elegir una opción");
+                in.nextLine();
+                error = true;
+            }
+        } while (error);
     }
 }
-            }
-                    }
